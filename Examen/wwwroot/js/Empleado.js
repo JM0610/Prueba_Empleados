be4e48713405e790a929ac778b2a992d7f5649e1
JESUS MARTINEZ <jesus.eduardo.ml@outlook.com>
2022-04-12 12:14:16 -0600

    muestra foto

--- Examen/wwwroot/js/Empleado.js
+++ Examen/wwwroot/js/Empleado.js
@@ -29,9 +29,9 @@ $('#tbl_empleado').on('click', '.btn_actualizar', function () {
     $('#Cedula').val($(this).closest('tr').find('td').eq(2).html())
     $('#Correo').val($(this).closest('tr').find('td').eq(3).html())
     $('#Fechanacimiento').val($(this).closest('tr').find('td').eq(11).html())
-    $('#Fechaingreso').val($(this).closest('tr').find('td').eq(10).html())
-    $('#slt_jefe').val($(this).closest('tr').find('td').eq(9).html())
-    $('#slt_area_emp').val($(this).closest('tr').find('td').eq(8).html())
+    $('#Fechaingreso').val($(this).closest('tr').find('td').eq(11).html())
+    $('#slt_jefe').val($(this).closest('tr').find('td').eq(10).html())
+    $('#slt_area_emp').val($(this).closest('tr').find('td').eq(9).html())
     
     console.log($(this).closest('tr').find('td').eq(6).html())
 });
